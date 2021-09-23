1c23a5397230fe077c4dc41c7d4150ac6ca26434
Ayomide Omolewa <ayomideomolewa@gmail.com>
2021-09-22 22:27:04 -0500

    Added more debugging

--- app.py
+++ app.py
@@ -28,14 +28,13 @@ def send_email(name, incoming_email, subject, body):
 	message['From'] = FROM
 	message['TO'] = TO
 	try:
-		print (1)
 		server = smtplib.SMTP("smtp.gmail.com", 587)
 		server.ehlo()
 		server.starttls()
-		print (2)
+		print (GMAIL)
+		print (GMAIL_PASS)
 		server.login(GMAIL, GMAIL_PASS)
 		server.send_message(message)
-		print (3)
 		server.close()
 		print ('successfully sent the mail')
 	except(err):
